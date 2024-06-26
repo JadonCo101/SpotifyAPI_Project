37b4c633d0263aeceb688d4bf605fb5f0427e59d
Jadon Co <102571575+JadonCo101@users.noreply.github.com>
2024-06-26 01:53:15 -0400

    Learning how to convert TypeScript to NodeJS

---
+++ SpotifyTinder/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+console.log(123);
+const addNumber = (a, b) => {
+    return a + b;
+};
+fs_1.default.writeFileSync("test.txt", "test");
+addNumber(1, 2);
